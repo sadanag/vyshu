23ec428896815213b17bbde556dec9df46396c40
nagaaaaaaaaaa <nagaprasad9048@gmail.com>
2024-08-23 22:40:11 +0530

    second

---
+++ addition.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number:5 "
+
+# Ask user for second number
+echo "Enter second number: 5"
+
+# Perform addition
+sum=$((5 + 5))
+
+# Display result
+echo "Sum: $sum"
+
