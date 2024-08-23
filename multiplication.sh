a358a36441d9af0360deb972b31ab1a6bf10f133
nagaaaaaaaaaa <nagaprasad9048@gmail.com>
2024-08-23 22:51:39 +0530

    fourth

---
+++ multiplication.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number:100"
+
+# Ask user for second number
+echo "Enter second number:1000 "
+
+# Perform multiplication
+prod=$((100 * 1000))
+
+# Display result
+echo "Product: $prod"
+
