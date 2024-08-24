a09dd629ac0a083e0e7bce119274492c0f17f6e2
unknown <nagaprasad9048@gmail.com>
2024-08-24 10:25:24 +0530

    frist

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number:67 "
+
+# Ask user for second number
+echo "Enter second number:90"
+
+# Perform addition
+sum=$((67 + 90))
+
+# Display result
+echo "$sum"
